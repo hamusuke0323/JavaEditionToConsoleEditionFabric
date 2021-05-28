9508458a1c94d854f69df5a33ed3db8db0d1347f
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2021-05-28 17:04:14 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JECE - Java Edition to Console Edition
+
+## License
+
+This mod is available under the MIT license.
---
+++ src/main/java/com/hamusuke/jece/client/MainClient.java
@@ -0,0 +1,9 @@
+package com.hamusuke.jece.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+    public void onInitializeClient() {
+
+    }
+}
